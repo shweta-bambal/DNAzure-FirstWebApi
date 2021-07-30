b06e6f6778dd4a92dee27ad1c6f99bb6893f033c
Shweta Bambal <75642571+shweta-bambal@users.noreply.github.com>
2021-07-30 17:20:59 +0530

    Update Startup.cs

--- MyDevopsWebApi/Startup.cs
+++ MyDevopsWebApi/Startup.cs
@@ -28,10 +28,10 @@ namespace MyDevopsWebApi
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyDevopsWebApi", Version = "v1" });
-            });
+           // services.AddSwaggerGen(c =>
+           // {
+             //   c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyDevopsWebApi", Version = "v1" });
+           // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +40,8 @@ namespace MyDevopsWebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyDevopsWebApi v1"));
+               // app.UseSwagger();
+               // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyDevopsWebApi v1"));
             }
 
             app.UseHttpsRedirection();
